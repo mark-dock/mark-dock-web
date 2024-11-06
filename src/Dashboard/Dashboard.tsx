5528066cfbfe2af3f8478399a5fc0e5b5804d578
AndrewWMeier <123119277+AndrewWMeier@users.noreply.github.com>
2024-11-05 16:37:57 -0800

    create orgs component made

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import axiosInstance from "../Config/axiosInstance";
-
+import CreateOrg from "../Components/CreateOrg";
 import Logout from "../Components/Buttons/Logout";
 
 export default function Dashboard() {
@@ -32,6 +32,7 @@ export default function Dashboard() {
                 )}
 
                 < Logout />
+                <CreateOrg />
             </div>
         </div>
     );
