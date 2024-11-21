4347e4277b327623c7177b6ed3917edff1bf7c37
AndrewWMeier <123119277+AndrewWMeier@users.noreply.github.com>
2024-11-20 18:30:13 -0800

    Started org settings UI (#9)
    * username added to manageorgs comp
    
    * org settings ui changes

--- src/UserSettings/UserSettings.tsx
+++ src/UserSettings/UserSettings.tsx
@@ -1,28 +1,14 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axiosInstance from "../Config/axiosInstance";
+import ManageOrgs from '../Components/ManageOrgs';
+
 
 export default function UserSettings() {
     return (
         <div>
             <h1>User Settings</h1>
 
-            <form>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" />
-                </div>
-
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" />
-                </div>
-
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
-                </div>
-
-                <button>Save</button>
-            </form>
+            <ManageOrgs />
         </div>
     );
 }
